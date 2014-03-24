b44c69f99bb56946f1837a732acb91eb9cdff266
adrian.frimpong <adrian.frimpong@gmail.com>
2014-03-24 02:15:05 -0400

    Completed pickler using TDD, removing compiled files, passing tests

--- pickle_test.py
+++ pickle_test.py
@@ -1,18 +1,24 @@
 import unittest
 
-from pickler import ListPickler,ListUnpickler
+from pickler import ListPickler, UnPickler
 
 class TestsForPickler(unittest.TestCase):
 	"""docstring for TestsForPickler"""
 	def setUp(self):
-		self.sample_pickle = [1,2,3]
+		self.file = "sample_pickle.p"
+		self.pickler = ListPickler()
+		self.unpickler = UnPickler()
+		# self.sample_pickle = [1,2,3]
 
 	def tearDown(self):
-		self.sample_pickle = None
+		self.pickler = None
+		self.unpickler = None
+		self.file = None
 
 	def assign_list_to_variable_test(self):
 		my_list = [1,2,3]
-		another_list = self.sample_pickle
+		self.pickler.pickle_list(my_list, self.file)
+		another_list = self.unpickler.unpickle_list(self.file)
 		self.assertEqual(my_list, another_list)
 		
 
--- pickler.py
+++ pickler.py
@@ -1,11 +1,19 @@
 import cPickle as pickle
+import os
 
 class ListPickler(object):
 	"""ListPickler that converts lists into pickled objects"""
-	def pickle_lists(self, list):
-		pass
+	def pickle_list(self, list_to_pickle, filename):
+		output = open(filename, "wb")
+		pickle.dump(list_to_pickle, output)
+		output.close()
 
-class ListUnpickler(object):
+class UnPickler(object):
 	"""ListPickler that converts lists into pickled objects"""
-	def unpickle_lists(self, list):
-		pass
+	def unpickle_list(self, filename):
+		output = open(filename, "rb")
+		new_list = pickle.load(output)
+		output.close()
+		os.remove(filename)
+		return new_list
+
