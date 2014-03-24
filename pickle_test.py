a307b35fef894c662118f90c87b20435de63e79f
adrian.frimpong <adrian.frimpong@gmail.com>
2014-03-24 01:34:06 -0400

    First working test for list pickling passing

--- pickle_test.py
+++ pickle_test.py
@@ -1,8 +1,18 @@
 import unittest
-from pickler import list_pickler
+
+from pickler import ListPickler,ListUnpickler
 
 class TestsForPickler(unittest.TestCase):
 	"""docstring for TestsForPickler"""
-	def assign_list_to_variable_test(self, arg):
-		super(TestsForPickler, self).__init__()
-		self.arg = arg
+	def setUp(self):
+		self.sample_pickle = [1,2,3]
+
+	def tearDown(self):
+		self.sample_pickle = None
+
+	def assign_list_to_variable_test(self):
+		my_list = [1,2,3]
+		another_list = self.sample_pickle
+		self.assertEqual(my_list, another_list)
+		
+
---
+++ pickler.py
@@ -0,0 +1,11 @@
+import cPickle as pickle
+
+class ListPickler(object):
+	"""ListPickler that converts lists into pickled objects"""
+	def pickle_lists(self, list):
+		pass
+
+class ListUnpickler(object):
+	"""ListPickler that converts lists into pickled objects"""
+	def unpickle_lists(self, list):
+		pass
